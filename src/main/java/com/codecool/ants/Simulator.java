66b04bce8420c4bad8c0fd1525aad7f289f6a1a3
Dariusz Okonski <darek200180@gmail.com>
2021-08-05 13:43:05 +0100

    working on project

--- src/main/java/com/codecool/ants/Simulator.java
+++ src/main/java/com/codecool/ants/Simulator.java
@@ -10,6 +10,7 @@ public class Simulator {
         colony.display();
 
         System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
 
     }
 
